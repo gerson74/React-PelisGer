e6eb2bcc9bfb7ad6d8cc6970b87e48c5cf3e4ad3
gerson74 <gersonlargo74@gmail.com>
2025-11-06 20:29:44 -0500

    actualizado

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Image, StyleSheet, Text, View } from 'react-native';
-import { Movie } from '../api/movies';
+import { Movie } from '../src/api/movies';
 
 type Props = {
   movie: Movie;
